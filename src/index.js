f3f13e44e5309d6fff49711367e4918250067c79
Manar <manar.qasem732016@gmail.com>
2022-10-12 20:18:05 +0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -3,26 +3,30 @@ import { leaderBoard } from './scores.js';
 import Scores from './show.js';
 
 const form = document.querySelector('.add-score');
-const refresh = document.querySelector('#refresh');
-const newUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg23Udz/scores/';
+const submit= document.querySelector('#btn');
+const refresh= document.querySelector('#refresh');
+const newUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/YWeqzTn9QGXjVzrbdb8U/scores/';
+
 const user = document.querySelector('.name');
 const score = document.querySelector('.score');
 
+
+
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  leaderBoard(user.value, Number(score.value));
-  form.reset();
-});
-
-refresh.addEventListener('click', async () => {
-  window.location.reload();
-  const storedScores = await fetch(newUrl);
-  const response = await storedScores.json();
-  Scores.displayScores(response.result);
-});
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const storedScores = await fetch(newUrl);
-  const response = await storedScores.json();
-  Scores.displayScores(response.result);
-});
+    e.preventDefault();
+    leaderBoard(user.value, Number(score.value));
+    form.reset();
+  });
+  
+  refresh.addEventListener('click', async () => {
+    window.location.reload();
+    const storedScores = await fetch(newUrl);
+    const response = await storedScores.json();
+    Scores.displayScores(response.result);
+  });
+  
+  document.addEventListener('DOMContentLoaded', async () => {
+    const storedScores = await fetch(newUrl);
+    const response = await storedScores.json();
+    Scores.displayScores(response.result);
+  });
